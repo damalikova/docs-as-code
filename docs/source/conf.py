546bf4d0565afed14cb708c86ed50a4b2829d49b
Daria Malikova <55386345+damalikova@users.noreply.github.com>
2023-10-25 15:18:33 +0300

    added some readthedocs template files

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,15 +9,27 @@
 project = 'Lumache'
 copyright = '2023, Graziella'
 author = 'Graziella'
+
 release = '0.1'
+version = '0.1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
     'sphinx_wagtail_theme',
-    'sphinx.ext.autodoc'
 ]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
 exclude_patterns = []
