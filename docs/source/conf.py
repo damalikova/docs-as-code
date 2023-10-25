08dbebcf78bf595b3023e612247743f01fdb333b
Daria Malikova <55386345+damalikova@users.noreply.github.com>
2023-10-25 17:04:47 +0300

    managed theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,6 +38,6 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinxawesome-theme'
+html_theme = 'sphinxawesome_theme'
 html_static_path = ['_static']
 # html_css_files = ["custom.css"]
